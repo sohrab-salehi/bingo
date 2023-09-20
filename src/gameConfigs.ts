82c138a3a7cfe206322da3bbc0bb72858f9c48ed
sohrab salehi <sohrab@sohrabs-MacBook-Air.local>
2023-09-20 13:30:08 +0200

    Build a simple bingo board

---
+++ src/gameConfigs.ts
@@ -0,0 +1,31 @@
+const configs = {
+  default: [
+    { id: 1, text: 'child noises in the background' },
+    { id: 2, text: 'Hello, hello?' },
+    { id: 3, text: 'I need jumb in another call' },
+    { id: 4, text: 'Can everyone go on mute' },
+    { id: 5, text: 'Could you please get closer to the mic' },
+    { id: 6, text: '(Load painful echo / feedback)' },
+    { id: 7, text: 'Next slide, please.' },
+    { id: 8, text: 'Can we take this offline?' },
+    { id: 9, text: 'Is _____ on the call?' },
+    { id: 10, text: 'Could you share this slides afterwards?' },
+    { id: 11, text: 'Can somebody grant presenter rights?' },
+    { id: 12, text: 'Can you email that to everyone?' },
+    { id: 13, text: 'CONF CALL BINGO' },
+    { id: 14, text: 'Sorry, I had problems loging in' },
+    { id: 15, text: '(Animal noises in the background)' },
+    { id: 16, text: "Sorry I didn't found the conference id" },
+    { id: 17, text: 'I was having connection issues' },
+    { id: 18, text: "I'll have to get back to you" },
+    { id: 19, text: 'Who just joined?' },
+    { id: 20, text: 'Sorry, something ____ with my calendar' },
+    { id: 21, text: 'Do you see my screen?' },
+    { id: 22, text: "Let's wait for ____!" },
+    { id: 23, text: 'You will send the minutes?' },
+    { id: 24, text: 'Sorry I was on mute.' },
+    { id: 25, text: 'Can you repeat please?' },
+  ],
+};
+
+export default configs;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import jsonConfig from '../gameConfigs';
+
+interface Cell {
+  id: number;
+  text: string;
+}
+
 function Home() {
-  return <div>This is homepage!</div>;
+  const createBoard = () => {
+    const board: JSX.Element[] = [];
+    const cells: Cell[] = jsonConfig.default;
+
+    for (let i = 0; i < 5; i += 1) {
+      const row = (
+        <div className="flex-container">
+          {cells.slice(i * 5, (i + 1) * 5).map((cell: Cell) => (
+            <div key={cell.id}>{cell.text}</div>
+          ))}
+        </div>
+      );
+      board.push(row);
+    }
+    return board;
+  };
+  return <div>{createBoard()}</div>;
 }
 
 export default Home;
