6cc8a986b2ae68e46eecdc1991198181302da2a5
sohrab salehi <sohrab@sohrabs-MacBook-Air.local>
2023-09-20 15:56:49 +0200

    Add functionality for selecting a cell

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,43 @@
+import { useState } from 'react';
 import jsonConfig from '../gameConfigs';
 
 interface Cell {
   id: number;
   text: string;
+  selected: boolean;
 }
 
 function Home() {
+  const [cells, setCells] = useState(jsonConfig.default);
+
+  const onClickCell = (cellId: number) => {
+    const updatedCell = cells.find((item) => item.id === cellId);
+    const newCells = cells.filter((item) => item.id !== cellId);
+    if (updatedCell) {
+      newCells.splice(updatedCell.id - 1, 0, {
+        ...updatedCell,
+        selected: true,
+      });
+    }
+    setCells(newCells);
+  };
+
   const createBoard = () => {
     const board: JSX.Element[] = [];
-    const cells: Cell[] = jsonConfig.default;
 
     for (let i = 0; i < 5; i += 1) {
       const row = (
         <div className="flex-container">
           {cells.slice(i * 5, (i + 1) * 5).map((cell: Cell) => (
-            <div key={cell.id}>{cell.text}</div>
+            <div key={cell.id}>
+              <button
+                className={cell.selected ? 'selected' : 'unselected'}
+                onClick={() => onClickCell(cell.id)}
+                type="button"
+              >
+                {cell.text}
+              </button>
+            </div>
           ))}
         </div>
       );
